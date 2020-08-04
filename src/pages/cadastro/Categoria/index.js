5f0ece39cfd94b0ab051f3047ea598787cce26b5
SalesLucas <lucasmarthaus04@gmail.com>
2020-08-04 15:51:13 -0300

    Resolvido json

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,18 +31,14 @@ function CadastroCategoria() {
   // ============
 
   useEffect(() => {
-    if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
-      fetch(URL)
-       .then(async (respostaDoServer) =>{
-        if(respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setCategorias(resposta);
-          return; 
-        }
-        throw new Error('Não foi possível pegar os dados');
-       })
-    }    
+     const URL = 'http://localhost:8080/categoria' 
+     fetch(URL)
+     .then(async(respostaDoServidor) => {
+       const resposta = await respostaDoServidor.json();
+       setCategorias([
+         ...resposta,
+       ]);
+     });
   }, []);
 
   return (
@@ -70,7 +66,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição:"
-          type="????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
